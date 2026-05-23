a675e9abfe05c85b3d443e255f9f90f319b1c759
2uptdsdn234barambang2-svg <2uptdsdn234barambang2@gmail.com>
2026-05-23 16:39:08 +0800

    Update Google Apps Script Web App URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxqaHSJh3T0J9Xu4sI1zp5idjXXrEvOJcYWmho456BIJ9yV2DtRL43aRbI-i0txKBR0Ug/exec"; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw2jeZKmZtNtPPuzeCcxMifKP0wooP2dne_9t72v-HazTS0lLVJRqXuGjaHBCQhn0ishA/exec"; 
 
 export interface FormField {
   id: string;
